af530cdc1f41780596f0afa2145ba701fb36dd5e
Lilian CIZERON <cizeron.lilian@hotmail.fr>
2021-04-11 17:22:32 +0200

    Before Todo List

--- src/view/Main.java
+++ src/view/Main.java
@@ -44,12 +44,40 @@ public class Main {
 		item[12] = new Wall(0, 13);
 		item[13] = new Wall(0, 14);
 		item[14] = new Wall(0, 0);
+		item[15] = new Fruit(10, 1, 1);
+		item[16] = new Fruit(10, 2, 1);
+		item[17] = new Fruit(10, 3, 1);
+		item[18] = new Fruit(10, 4, 1);
+		item[19] = new Fruit(10, 5, 1);
+		item[20] = new Fruit(10, 6, 1);
+		item[21] = new Fruit(10, 7, 1);
+		item[22] = new Fruit(10, 8, 1);
+		item[23] = new Fruit(10, 9, 1);
+		item[24] = new Fruit(10, 10, 1);
+		item[25] = new Fruit(10, 11, 1);
+		item[26] = new Fruit(10, 12, 1);
+		item[27] = new Fruit(10, 13, 1);
+		item[28] = new Wall(14, 1);
+		item[29] = new Wall(0, 0);
+		item[30] = new Wall(1, 0);
+		item[31] = new Wall(2, 0);
+		item[32] = new Wall(3, 0);
+		item[33] = new Wall(4, 0);
+		item[34] = new Wall(5, 0);
+		item[35] = new Wall(6, 0);
+		item[36] = new Wall(7, 0);
+		item[37] = new Wall(8, 0);
+		item[38] = new Wall(9, 0);
+		item[39] = new Wall(10, 0);
+		item[40] = new Wall(11, 0);
+		item[41] = new Wall(12, 0);
+		item[42] = new Ghost(13, 1);
 		
-		int k = 15;
+		int k = 43;
 		
 		for (int i = 1; i < taille; i++) {
 			
-			for (int j = 0; j < taille; j++) {
+			for (int j = 2; j < taille; j++) {
 				
 				item[k] = new Wall(i, j);
 				k = k + 1;
