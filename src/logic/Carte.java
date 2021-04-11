bbc930f46813353996b11efa52ec7682b8e53fa4
Camillou-brr <camille.barbarin@hotmail.com>
2021-04-11 17:05:00 +0200

    move_Ghost

--- src/logic/Carte.java
+++ src/logic/Carte.java
@@ -1,6 +1,7 @@
 package logic;
 
 import data.Entite;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Class Carte
@@ -223,7 +224,7 @@ public class Carte implements Interface_VL{
 	
 	/**
 	 * Getter for PACMAN_POSITION attribute of carte
-	 * @return
+	 * @return position an integer
 	 */
 	public int get_position() {
 		return this.PACMAN_POSITION;
@@ -242,9 +243,15 @@ public class Carte implements Interface_VL{
 					if (is_a_wall(liste[i]) == false) {
 						((Ghost) liste[i]).move_ghost(liste);
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
+					// Si la direction du fantome n'est pas un valide (= c'est un mur) on cherche une autre direction pour le fantome, tant que la direction suivante est un mur ET que il n'y a pas une direction linéaire ET tant que l'on ne bouge pas ET que on est pas out of bounds
+					while(is_a_wall(liste[i]) && (((Ghost) liste[i]).get_direction_x() != 0 && ((Ghost) liste[i]).get_direction_y() != 0) && (((Ghost) liste[i]).get_direction_x() == 0 && ((Ghost) liste[i]).get_direction_y() == 0) && (liste[i].get_x() + ((Ghost) liste[i]).get_direction_x() < 0 && liste[i].get_y() + ((Ghost) liste[i]).get_direction_x() > 14 && liste[i].get_y() + ((Ghost) liste[i]).get_direction_y() < 0 && liste[i].get_y() + ((Ghost) liste[i]).get_direction_y() > 14)) {
+						((Ghost) liste[i]).set_direction_x(ThreadLocalRandom.current().nextInt(-1, 1));
+						((Ghost) liste[i]).set_direction_y(ThreadLocalRandom.current().nextInt(-1, 1));
+					} 
 				}
+				catch (Exception e) {
+				e.printStackTrace();
+				}				
 			}
 		}
 	}
