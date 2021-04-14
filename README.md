d61619d386c6b0cabcce4365d5756fcfff962bf2
Lilian CIZERON <cizeron.lilian@hotmail.fr>
2021-04-14 20:53:56 +0200

    End Project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-PacmanBis
+#PacmanBis
+## Authors : Lilian Cizeron & Camille Barbarin--Renvoisé & Antoine Chrétin
+
+### How to ?
+Binary can be found under release/
+
+Launch with `java -jar Pacman_v1.1.jar`
+
+###Q&A
+
+**Why the console display many NullPointerException ?**
+Then, it is because of a method use to display ImageIcon.
+
+Don't worry about it, it is fully under control
--- src/view/Audio.java
+++ src/view/Audio.java
@@ -7,31 +7,37 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+/**
+ * Audio Class to play sound in the game
+ * Code is mostly taken on internet
+ * @author Lilian Cizeron
+ *
+ */
 public abstract class Audio{
-	
-	static boolean play = true;
 	
 	// Fonction de l'audio du jeu
 	public static synchronized void playSound(final String url) {
+	// Lancement d'un thread pour ne pas bloquer le programme pendant la lecture du fichier son
 	  new Thread(new Runnable() {
 	    public void run() {
 	      try {
-	    	 play = true;
+	    	  
+	    	 // Ouverture du fichier son
 	    	 InputStream audioSrc = getClass().getResourceAsStream("/view/" + url);
 	         Clip clip = AudioSystem.getClip();
+	         
+	         // Mise en buffer (requis pour avoir le son dans le .jar)
 	         InputStream bufferedIn = new BufferedInputStream(audioSrc);
 	         AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+	         
+	         // Lancement du son
 	         clip.open(audioStream);
 	         clip.start(); 
-	      } catch (Exception e) {
+	         
+	      } 
+	      catch (Exception e) {
 	      }
 	    }
 	  }).start();
-	}
-	
-	public static synchronized void stop() {
-		
-		play = false;
-		
 	}
 }
